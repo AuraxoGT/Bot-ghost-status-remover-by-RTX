b9e034a38eb4b51e264a4ddfb03d5d6e7a54f394
AuraxoGT <135165346+AuraxoGT@users.noreply.github.com>
2024-03-12 19:17:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Draugelis","Made By AuraxoGT"];
 
 
 let currentIndex = 0;
