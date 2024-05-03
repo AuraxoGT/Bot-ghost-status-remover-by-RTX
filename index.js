4fe8daa34ff2437a2dd5f7e341396bb03e0f7878
AuraxoGT <135165346+AuraxoGT@users.noreply.github.com>
2024-05-04 02:58:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Made By AuraxoGT","MAFIA"];
+const statusMessages = ["Made By AuraxoGT","MAFIA","Los Angel RP"];
 
 
 let currentIndex = 0;
@@ -98,7 +98,7 @@ client.once('ready', () => {
 
   setInterval(() => {
     updateStatusAndSendMessages();
-  }, 1000);
+  }, 10000);
 });
 
 login();
