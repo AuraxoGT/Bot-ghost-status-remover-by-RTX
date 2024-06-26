c7a89f1697f0b33a0d980c6e93c0aeec5c17fa8a
AuraxoGT <135165346+AuraxoGT@users.noreply.github.com>
2024-06-26 15:28:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Made By AuraxoGT","ARAS","XC LEGACY RP","DM For Problems"];
+const statusMessages = ["Made By AuraxoGT","PD","XC LEGACY RP","DM For Problems"];
 
 
 let currentIndex = 0;
